1d8d404e3b78f5aa6252bb6201a63f37ce0fb062
BWFXMUSIC <153865506+BWFXMUSIC@users.noreply.github.com>
2024-06-06 12:10:09 +0530

    [🔊♦️ᴀʟʟ🔇

--- BWFMUSIC/plugins/admins/skip.py
+++ BWFMUSIC/plugins/admins/skip.py
@@ -9,7 +9,7 @@ from BWFMUSIC.core.call import rj
 from BWFMUSIC.misc import db
 from BWFMUSIC.utils.database import get_loop
 from BWFMUSIC.utils.decorators import AdminRightsCheck
-from Romeo.utils.romeomusic.rj import command
+from Romeo.utils.bwfmusic.rj import command
 from BWFMUSIC.utils.inline.play import (stream_markup,
                                           telegram_markup,
                                           close_keyboard)
