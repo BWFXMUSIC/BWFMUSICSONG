cf0a117d774cbf8af1e85a0d04f4cbdab1706e26
BWFXMUSIC <153865506+BWFXMUSIC@users.noreply.github.com>
2024-06-06 14:05:42 +0530

    [🔊ʙᴀɴ♦️ᴀʟʟ🔇

--- helper/helpers.py
+++ helper/helpers.py
@@ -167,7 +167,7 @@ HELP_11 = """😅**<u> started with bot</u>**
 
 /settings - Get a complete group's settings with inline buttons.
 
-/sudolist - Check Sudo Users of romeomusicBot"""
+/sudolist - Check Sudo Users of bwfmusicBot"""
 
 HELP_12 = """👤**<u>GBAN FUNCTION:</u>**
 
